a3ca89cea8392d367bdda60f8c844ffb922236dd
Luke_Skywalker <62883114+Arsh87474@users.noreply.github.com>
2020-04-27 23:04:11 +0530

    Update and rename Emoji_convertor_in_Python to Emoji_convertor.py

--- Emoji_convertor.py
+++ Emoji_convertor.py
@@ -1,10 +1,13 @@
 message = input(">  ")
 words = message.split(' ')
 emojis = {
-    ":)": "😀",
-    ":(": "☹️"
+    ":)": "🙂",
+    ":(": "☹️",
+    ":p": "🤪",
+    ":D": "😃"
 }
 output = ""
 for word in words:
    output += emojis.get(word, word) + " "
 print(output)
+
